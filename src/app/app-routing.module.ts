5e8fe1806dd3e2b6a98706f3540ad34b8bc16335
Chrhouma <charfeddine91@hotmail.fr>
2018-06-25 00:51:56 +0200

    il manque la liste des enregistremets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@
  * Created by front-vendeur on 22/06/18.
  */
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule, } from '@angular/router';
+import {Routes, RouterModule, Router} from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { SenariosComponent } from './senarios/senarios.component';
+import { SenarioComponent } from './senario/senario.component';
 const routes: Routes = [
     {
         path: '',
@@ -18,10 +19,20 @@ const routes: Routes = [
     {
         path: 'senarios',
         component: SenariosComponent
+      /*  children: [
+
+                { path: 'senario', component: SenarioComponent }
+
+        ]*/
+    },
+    {
+        path: 'senarios/:id',
+        component: SenarioComponent
     }
-];
+   ];
 @NgModule({
     imports : [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
 export class AppRoutingModule {}
+
