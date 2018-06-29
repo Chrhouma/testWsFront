b5379cccee4d669509a618dbb6480749cc8c286c
RhoumaC <Charfeddine.Rhouma@pictime-groupe.com>
2018-06-29 17:43:33 +0200

    lot1

--- src/app/webservices/webservices.component.ts
+++ src/app/webservices/webservices.component.ts
@@ -9,20 +9,35 @@ import {Router} from '@angular/router';
 })
 export class WebservicesComponent implements OnInit {
 datas: any;
+datainfo: any;
   constructor(private  httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.getWebServices();
+
   }
- getWebServices() {
+    getWebServices() {
     this.httpClient.get('http://localhost:8889/webServices')
         .subscribe(webServices => {
-          this.datas = webServices['webservices'];
+           this.datas = webServices['webservices'];
           console.log(webServices);
-        });
+        },
+            error => {
+
+            },
+            () => {
+                this.getinfo();
+            });
  }
   onSelect(event: string) {
     this.router.navigate(['webServices', event]);
+  }
+  getinfo() {
+     this.httpClient.get('http://localhost:8889/webServices/info')
+         .subscribe(info => {
+             this.datainfo = info['infogenerale'];
+             console.log(info);
+         });
   }
 
 }
